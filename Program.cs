b1279b3bc050fbd6edb9e532500f3e848b13e303
LeoVale <151870779+leoRDvale@users.noreply.github.com>
2024-04-23 21:32:09 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero:64981819090, modelo:"Lumia 720", imei:01234567890, memoria:128);
+nokia.Ligar();
+nokia.InstalarAplicativo("GitHub");
+
+Console.WriteLine("\n");
+Console.WriteLine("-------------------------------");
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero:64981510000, modelo:"13 Pro Max", imei:09876543210, memoria:128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Discord");
